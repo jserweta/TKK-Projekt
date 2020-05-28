27d5e105896d1016dda25836c3930a8be403a90d
jserweta <jakub.serweta@gmail.com>
2020-05-28 17:35:04 +0200

    res

--- src/agh/edu/pl/Main.java
+++ src/agh/edu/pl/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         String test = "{ \"brand\": \"Maserati\",  \"model\" \n: \"Quattroporte\",\"aaa\":\"fsagda\", \"owner\": {\"name\":\"Sophia\", \"age\": -1.2e1, \"married\": false}, \"mileages\": [1,2,{\"aa\":3}]}";
-
+                
         JSONReader reader = new JSONReader();
 
         try {
